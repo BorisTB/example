bde940bce86661564d56351e1b7f1a3283463744
Boris Polak <boris.polak@techfides.cz>
2018-02-27 11:27:36 +0100

    Set login fields as required

--- src/layouts/Public/LoginForm/LoginForm.js
+++ src/layouts/Public/LoginForm/LoginForm.js
@@ -3,8 +3,8 @@ import styles from './LoginForm.css'
 
 export default ({ onSubmit }) => (
   <form onSubmit={ onSubmit } className={ styles.form }>
-    <input className={ styles.input } type="text" name="username" placeholder="Username" />
-    <input className={ styles.input } type="password" name="password" placeholder="Password" />
+    <input className={ styles.input } required type="text" name="username" placeholder="Username" />
+    <input className={ styles.input } required type="password" name="password" placeholder="Password" />
     <button className={ styles.button } type="submit">Login</button>
   </form>
 )
